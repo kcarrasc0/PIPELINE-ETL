ce2328cd825bd33a6c19ba7e580b1757a640f478
Dandara Gouveia <131200312+8dandara@users.noreply.github.com>
2025-05-05 14:37:36 +0000

    refgrer

--- etl_musica_video/extracao_spotify.py
+++ etl_musica_video/extracao_spotify.py
@@ -4,22 +4,29 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 # Autenticação
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-    client_id="b5378e3b030a47908ca495535af59478",
-    client_secret="5ad53c1acc8f4995b9b7f0a65c4be670"
+    client_id="02a8e2e4f3714d97ba1ade9f3af1d539",
+    client_secret="75609231399b472fa745205adf602319"
 ))
 
-# Playlist "Top Brasil"
-playlist_id = "7xwSv337EetpUTnQLgxFbW"  # Top Músicas da playlist Thi is Souto MC
+# Playlist 
+playlist_id = "31VkXgAcOZSmt5wIL1TkkR"  # Top Músicas da playlist 年2000 
 result = sp.playlist_items(playlist_id, limit=20)
 
 musicas = []
+
 for item in result['items']:
     track = item['track']
+
+    nome_musica = track.get('name', 'Desconhecido')
+    nome_artista = track.get('artists', [{}])[0].get('name', 'Desconhecido')
+    popularidade = track.get('popularity', 0)
+
     musicas.append({
-        "nome": track['name'],
-        "artista": track['artists'][0]['name'],
-        "popularidade": track['popularity']
+        "nome": nome_musica,
+        "artista": nome_artista,
+        "popularidade": popularidade
     })
+
 
 df = pd.DataFrame(musicas)
 df.to_csv("spotify_data.csv", index=False)
--- etl_musica_video/painel.py
+++ etl_musica_video/painel.py
@@ -3,21 +3,13 @@ import streamlit as st
 from PIL import Image
 
 # Título
-st.title("🎶 Análise de Tendências: Spotify & YouTube")
+st.title("Top Músicas da playlist 年2000 no spotify")
 
 # Carregar dados
 df_spotify = pd.read_csv("spotify_data.csv")
 df_youtube = pd.read_csv("youtube_data.csv")
 
-# Carregar imagem
-img = Image.open("ALBUM SOUTO MC.png")
 
-# Layout com colunas: imagem ao lado do título
-col1, col2 = st.columns([1, 4])
-with col1:
-    st.image(img, width=100)
-with col2:
-    st.header("Top músicas da Playlist THIS IS SOUTO MC no Spotify")
 
 # Mostrar tabela Spotify
 st.dataframe(df_spotify)

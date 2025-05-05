ef9ce5653e6e4c24a5abd27e3690d4940df57500
lucasalxF <lucasalx.ferreira@gmail.com>
2025-05-05 13:21:12 +0000

    adicionando minha playlist

--- etl_musica_video/extracao_youtube.py
+++ etl_musica_video/extracao_youtube.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from googleapiclient.discovery import build
 
-api_key = "AIzaSyCPaB239KDMSNPYHRigJxKRygdqeDc0yYE"
+api_key = "AIzaSyCMC6SPFRHATNY8QsCqtVepHE4TS1pYmnE"
 youtube = build("youtube", "v3", developerKey=api_key)
 
 request = youtube.videos().list(
